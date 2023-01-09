34a2aeeadb434aac88b71230f8c94c412eb373ed
Reza Ghasemi <116528459+Reza-CSEE@users.noreply.github.com>
2023-01-09 14:39:16 -0500

    Delete Vowel-Letters.py

--- Vowel-Letters.py
+++
@@ -1,7 +0,0 @@
-i =input('Please enter your sentence to recognize the number of vowel letters:\n')
-count=0
-for w in i:
-    if w=='a' or w=='e' or w=='i' or w=='o' or w=='u':
-        count+=1
-
-print(count)
